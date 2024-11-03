de3b4aec1c7362a5f275af91ef890a9a4b8447da
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-03 18:47:03 +0100

    added barebone navbar

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,15 +1,42 @@
 <template>
-  <nav></nav>
+  <nav>
+    <div>
+      <a href="">vb2007.hu</a>
+      <button class="navbar-toggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="/" class="nav-link"></a>
+          </li>
+          <li class="navbar-item">
+            <a href="/" class="nav-link"></a>
+          </li>
+          <li class="navbar-item">
+            <a href="/" class="nav-link"></a>
+          </li>
+        </ul>
+        <div class="nav-list-right"></div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
 nav {
-  display: none;
+  background-color: rgb(18, 22, 29);
 }
 
 .nav-list {
 }
 
+.nav-list-right {
+}
+
 .nav-item {
+}
+
+.nav-link {
 }
 </style>
