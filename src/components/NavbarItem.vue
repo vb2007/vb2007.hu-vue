b3d9d530fcc40f914f18d767e8ec5c44a084bfd6
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-06 23:00:47 +0100

    modified nav display and class naming style

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,59 +1,76 @@
 <template>
   <nav>
-    <a href="">vb2007.hu</a>
-    <button class="nav-toggler">
-      <span class="nav-toggler-icon"></span>
-    </button>
-    <div class="nav-collapse">
-      <ul class="nav-list">
-        <li class="nav-home">
-          <a href="/" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item">
-          <a href="/" class="nav-link">Shorten</a>
-        </li>
-        <li class="nav-item">
-          <a href="/" class="nav-link">Pastebin</a>
-        </li>
-        <li class="nav-item">
-          <a href="/" class="nav-link">Upload</a>
-        </li>
-        <li class="nav-item">
-          <a href="/" class="nav-link">Contact</a>
-        </li>
-      </ul>
-      <div class="nav-list-right"></div>
+    <div class="nav--box">
+      <a class="nav--title" href="">vb2007.hu</a>
+      <button class="nav--toggler">
+        <span class="nav--toggler-icon"></span>
+      </button>
+      <div class="nav--collapse">
+        <ul class="nav--list">
+          <li class="nav--home">
+            <a href="/" class="nav--link">Home</a>
+          </li>
+          <li class="nav--item">
+            <a href="/" class="nav--link">Shorten</a>
+          </li>
+          <li class="nav--item">
+            <a href="/" class="nav--link">Pastebin</a>
+          </li>
+          <li class="nav--item">
+            <a href="/" class="nav--link">Upload</a>
+          </li>
+          <li class="nav--item">
+            <a href="/" class="nav--link">Contact</a>
+          </li>
+        </ul>
+        <div class="nav--list-right"></div>
+      </div>
     </div>
   </nav>
 </template>
 
 <style scoped>
 nav {
+  background-color: rgb(18, 22, 29);
+  height: 60px;
+  width: 100%;
   display: flex;
   justify-content: center;
-  overflow: hidden;
+  align-items: center;
+  /* overflow: hidden; */
   position: fixed;
-  right: 0;
   top: 0;
-  width: 100%;
-  background-color: rgb(18, 22, 29);
+  /* width: 100%; */
 }
 
-.nav-list {
+.nav--box {
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  max-width: 1300px;
+  padding-right: 50px;
+  padding-left: 50px;
+  z-index: 1;
+}
+
+.nav--title {
+}
+
+.nav--list {
   display: flex;
   list-style: none;
   padding: 0;
   margin: 0;
 }
 
-.nav-list-right {
+.nav--list-right {
 }
 
-.nav-item {
+.nav--item {
   margin-right: 20px;
 }
 
-.nav-link {
+.nav--link {
   color: white;
   text-decoration: none;
 }
