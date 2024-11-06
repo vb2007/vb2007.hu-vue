e83bb04adc0b0f7cbd6fae76338e9a1b0254495c
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-06 23:42:25 +0100

    added FooterItem

---
+++ src/components/FooterItem.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer>
+    <!-- TODO: automatically replace the second year with the current one -->
+    <p>VB2007 | 2023 - 2024</p>
+  </footer>
+</template>
+
+<style scoped></style>
