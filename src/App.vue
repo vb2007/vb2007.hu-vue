eae626bcac0b057ec4a215d7dd6a537509001b37
VB2007 <admin@vb2007.hu>
2024-12-27 16:17:41 +0100

    deleted extra css

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,15 @@
 <script setup lang="ts">
 import NavbarItem from './components/NavbarItem.vue'
+import WelcomeItem from './components/WelcomeItem.vue'
 import FooterItem from './components/FooterItem.vue'
 </script>
 
 <template>
   <NavbarItem />
 
+  <WelcomeItem />
+
   <FooterItem />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-:deep(body) {
-  position: relative;
-  min-height: 100vh;
-  padding-bottom: 60px; /* Same as footer height */
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
