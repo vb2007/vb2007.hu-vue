7eb811047f304e20736c7a625cf67246d64761ee
VB2007 <admin@vb2007.hu>
2024-12-27 15:44:51 +0100

    updated footer design

--- src/components/FooterItem.vue
+++ src/components/FooterItem.vue
@@ -1,6 +1,8 @@
 <template>
   <footer>
-    <p>VB2007 | 2023 - {{ currentYear }}</p>
+    <div class="footer--box">
+      <p>VB2007 | 2023 - {{ currentYear }}</p>
+    </div>
   </footer>
 </template>
 
@@ -18,13 +20,22 @@ export default {
 
 <style scoped>
 footer {
+  position: sticky;
+  top: 0;
+  background-color: rgba(219, 219, 219, 0.8);
+  box-shadow: 0 2px 10px rgb(253, 251, 251);
+  z-index: 1000;
+  margin: 0 12%;
+  /* for round top corners */
+  border-radius: 15px 15px 0 0;
+  text-align: center;
+  font-family: Inter, sans-serif;
+}
+
+.footer--box {
   display: flex;
   justify-content: center;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 60px;
-  background-color: rgb(131, 111, 111);
+  align-content: center;
+  padding-top: 10px;
 }
 </style>
