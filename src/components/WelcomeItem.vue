3f8431ccbd27fe18d7a617af160ff90528edef66
VB2007 <admin@vb2007.hu>
2024-12-27 16:03:37 +0100

    added welcome element

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="welcome--box">
+    <h1>Welcome home</h1>
+    <h2>Meet a feature-rich site with tools for your various needs!</h2>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.welcome--box {
+}
+</style>
