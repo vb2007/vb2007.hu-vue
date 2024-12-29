f6db51e7dc499639c6e87af81fc0dbcd84330bba
VB2007 <admin@vb2007.hu>
2024-12-29 01:30:53 +0100

    made the url shortening component

---
+++ src/components/pages/ShortenItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="shorten-container">
+    <h1>URL Shortener</h1>
+    <input v-model="longUrl" placeholder="Enter URL here" />
+    <button @click="shortenUrl">Shorten</button>
+    <div v-if="shortenUrl">
+      <p>Shortened URL: <a :href="shortUrl" target="_blank" {{ shortUrl }}></a></p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const longUrl = ref('')
+const shortUrl = ref('')
+
+const shortenUrl = async () => {
+  try {
+    const response = await axios.post('endpoint', { url: longUrl.value })
+    shortUrl.value = response.data.shortUrl
+  } catch (error) {
+    console.error(`Error while trying to shorten URL: ${error}`)
+  }
+}
+</script>
