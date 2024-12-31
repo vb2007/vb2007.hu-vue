1342dd5a31c22d5cb5524ea47aff13595eade40a
VB2007 <admin@vb2007.hu>
2024-12-31 07:34:51 +0100

    imported back previous elements, updated router links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavbarItem from './components/NavbarItem.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <NavbarItem />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
---
+++ src/components/icons/NavbarDropdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    class="iconify iconify--mdi"
+    width="800px"
+    height="800px"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M4 17H20M4 12H20M4 7H20"
+      stroke="#ffffff"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="currentColor"
+    ></path>
+  </svg>
+</template>
