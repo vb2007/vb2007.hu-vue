42f3fd9074b0d14ef783729433f5e5cdb4b1b67a
VB2007 <admin@vb2007.hu>
2025-08-22 01:10:03 +0200

    created api consants

---
+++ src/constants/api.ts
@@ -0,0 +1,14 @@
+const API_BASE_URL: string = import.meta.env.API_BASE_URL || "http://localhost:3000/";
+
+export class UserManagement {
+  static Authentication = class {
+    static register: string = `${API_BASE_URL}/auth/register`;
+    static login: string = `${API_BASE_URL}/auth/login`;
+  };
+
+  static Actions = class {
+    static getUsers: string = `${API_BASE_URL}/users`;
+    static getUser: string = `${API_BASE_URL}/user`;
+    static deletePatchUser: string = `${API_BASE_URL}/users/:id`;
+  };
+}
